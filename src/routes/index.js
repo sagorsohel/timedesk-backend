f639a44c71fdad717d578bd5a06d636e1b6b60d5
sagorsohel <sohel19sagor@gmail.com>
2025-08-08 21:17:39 +0600

    Update API route paths for stats and badge endpoints in index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,8 +10,8 @@ import badgeRouter from "./badge.route.js";
 
 const router = express.Router();
 
-router.use("/api/stats", statsRouter);    // /api/stats/:userId
-router.use("/api/badge", badgeRouter);    // /api/badge/:userId and /api/badge/graph/:userId
+router.use("/stats", statsRouter);    // /api/stats/:userId
+router.use("/badge", badgeRouter);    // /api/badge/:userId and /api/badge/graph/:userId
 router.use("/auth", authRouter);
 router.use("/user", usersRouter);
 router.use("/routines",authMiddleware, routineRouter);
